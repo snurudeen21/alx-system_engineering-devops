283e13658fd434e82f7857222a43e367b2658314
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-01 07:30:36 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 
 exec { 'install_flask':
   command  => '/usr/bin/pip3 install Flask==2.1.0',
-  unless   => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  unless   => '/usr/bin/pip3 show Flask | grep -q "Version: $(URI::encode_www_form_component("2.1.0"))"',
   provider => 'shell',
 }
