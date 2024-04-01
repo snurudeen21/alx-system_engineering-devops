4523e5343363c7c5485c5c8fabfd9bcce782bab7
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-01 06:56:45 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# install Flask using puppet
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Using Puppet to kill a process called killmenow
-
-exec { 'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
