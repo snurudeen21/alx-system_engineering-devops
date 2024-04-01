eb2716d2085c2ad775a10688b40bd4880d19d309
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-01 07:32:24 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # install Flask using puppet
 
-exec { 'install_flask':
-  command  => '/usr/bin/pip3 install Flask==2.1.0',
-  unless   => '/usr/bin/pip3 show Flask | grep -q "Version: $(URI::encode_www_form_component("2.1.0"))"',
-  provider => 'shell',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
