bde0540f6209d6be177bc101b8033d5c2bd87e43
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-01 07:35:30 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # install Flask using puppet
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+  provider => 'pip3',
 }
