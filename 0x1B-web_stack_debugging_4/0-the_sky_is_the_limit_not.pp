6cfdcbcb13c762253fa954c7c8d5a8beb328e98f
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-05-14 23:57:53 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Configure nginx to allow more requests
-# webstack debugging
+# webstack debugging by puppet
 exec { 'fix--for-nginx':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
